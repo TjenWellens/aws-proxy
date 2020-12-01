59cf2fe9b55a6cda42910aaf889ad314880e55f9
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-01 22:20:04 +0100

    reset code so people only see readme and don't start at the last step

--- route53-zone-data.tf
+++
@@ -1,6 +0,0 @@
-// the zone should have been created in the previous step
-data "aws_route53_zone" "MyHttpProxy" {
-  name         = var.domain_name
-  private_zone = false
-  depends_on = [aws_route53_zone.primary]
-}
